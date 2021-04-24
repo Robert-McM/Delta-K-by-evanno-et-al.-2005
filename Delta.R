b222e0f42de61c3b53539abab91ee111a5b8af0c
Robert-McM <59290587+Robert-McM@users.noreply.github.com>
2021-04-23 20:41:17 -0500

    Add files via upload

---
+++ Delta.R
@@ -0,0 +1,42 @@
+
+##This function calculates optimal K using the method described by evanno et al. 2005
+#Requires a log probability file with two columns, V1 being the value of K, 
+#and V2 being the LnProb given by STRUCTURE
+library(dplyr)
+library(ggplot2)
+
+x<- read.table("/Users/dj/CLUMPAK_data/Other/Log_Prob2.txt")
+y<- read.table("/Users/dj/CLUMPAK_data/Other/Log_Prob_same.txt")
+Delta_K(x)
+
+Delta_K<- function(x) {
+  T2<- x %>% group_by(V1) %>% summarise(St_dev= sd(V2), mean=mean(V2))
+  T3<- T2 %>% filter(V1!=1) %>% select(mean)
+  T3<- as.data.frame(c(0,T2$mean[-1]))
+  T2<- T2[-3]
+  T2<- cbind(T2,T3)
+  names(T2)[3]<- "mean"
+  T2<- T2 %>% mutate(LK1=0, LK2=0)
+  
+  for (i in 2:length(T2$V1)){
+    T2$LK1[i]<- T2$mean[i]-T2$mean[i-1]
+  }
+  
+  for (i in 2:length(T2$V1)){
+    T2$LK2[i]<- T2$LK1[i+1]-T2$LK1[i]
+  }
+  
+  T2<- T2 %>% mutate(delta_k=abs(LK2)/St_dev)
+  
+  g<- ggplot(T2, aes(x=V1, y=delta_k))+
+    geom_bar(aes(y=delta_k), stat = "identity", fill="#E3FF47", alpha=0.85)+
+    geom_point(col="#6347FF")+
+    geom_line(col="#6347FF")+
+    theme_bw()+
+    labs(title = expression(paste(italic("Silphium albiflorum"), " SNP Genotyping")), 
+         subtitle = expression(paste("Optimal Number of Clusters [",Delta,"K=m(","|","L''K","|",")","/",sigma,"L(K)]")), 
+         y= expression(paste(Delta, "K")), 
+         x="K")+
+    scale_x_continuous(breaks = seq(1,length(T2$delta_k)))
+  return(g)
+}
